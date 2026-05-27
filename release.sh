305e04b0261d82c0c08f9c3f7f9e1b8553d18fa5
codeptr <i@qibu.space>
2026-05-28 01:15:06 +0800

    Remove unnecessary code in release.sh

--- release.sh
+++ release.sh
@@ -18,17 +18,15 @@ osarch="!darwin/arm !darwin/386"
 
 echo "Compiling:"
 
-os="windows linux darwin"
-arch="amd64 386 arm arm64 mips mips64 mipsle mips64le"
+os="windows linux"
+arch="amd64"
 pushd cmd/ck-client
 CGO_ENABLED=0 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarch="$osarch" -output="$output"
-CGO_ENABLED=0 GOOS="linux" GOARCH="mips" GOMIPS="softfloat" go build -ldflags "-X main.version=${v}" -o ck-client-linux-mips_softfloat-"${v}"
-CGO_ENABLED=0 GOOS="linux" GOARCH="mipsle" GOMIPS="softfloat" go build -ldflags "-X main.version=${v}" -o ck-client-linux-mipsle_softfloat-"${v}"
 mv ck-client-* ../../release
 popd
 
 os="linux"
-arch="amd64 386 arm arm64"
+arch="amd64"
 pushd cmd/ck-server
 CGO_ENABLED=0 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarch="$osarch" -output="$output"
 mv ck-server-* ../../release
